896c7e65d133530e6173345eb9bb53eeb1784ca1
Nikola Tesla <Nikola Tesla>
2016-02-10 20:00:58 +0400

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,9 +1,19 @@
+import static java.lang.Math.*;
+
 public class Calculate {
 	
 		public static void main(String[] arg) {
 			
 			System.out.println("Calculate...");
 			
+			Double a = Double.valueOf(arg[0]);
+			Double b = Double.valueOf(arg[1]);
+			
+			System.out.println ("Сумма " +  ( a + b));
+			System.out.println ("Разница " +  ( a - b));
+			System.out.println ("Произведение " +  ( a * b));
+			System.out.println ("Деление " +  ( a / b));
+			System.out.println("Степень " + (pow(a,b)));
 		}
 		
 }
